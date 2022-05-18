c5d34bd966fa1c5768403aa5b1236693888c5d4a
Jessica Pansini <68460585+jspansini@users.noreply.github.com>
2022-05-18 02:42:12 -0300

    Add files via upload
    Calculando a hora trabalhada

---
+++ CalculoPorHora/funcionario/Funcionarios.java
@@ -0,0 +1,43 @@
+package funcionario;
+
+public class Funcionarios{
+	int id;
+	String nome;
+	String cargo;
+	double salario;
+	
+	public double calcularSalario(int hora, double valorHora){
+		double salario = hora * valorHora;
+		return salario;
+		
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public Double getSalario() {
+		return salario;
+	}
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+	
+
+}
